e35487c1946d9c4f4a947ab01b85f0588163b5a5
joojo-sena <joaosenaxd@gmail.com>
2025-01-31 02:54:07 -0300

    Java Introduction

---
+++ HellloWorld.java
@@ -0,0 +1,8 @@
+package secao1;
+
+public class HellloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Im joojo, and I will be doing some stuff with Java :)");
+    }
+}
